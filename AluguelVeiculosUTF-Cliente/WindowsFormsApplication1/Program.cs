a811959212ddf8d4a495dda7feac433840d0e53e
maykooliver <maykooliver@gmail.com>
2015-06-15 18:56:31 -0300

    Revert "Revert "Revert "Atualizando Telas C#"""
    This reverts commit 7b7df3a2eb3a069b24e9c3e4da51f358d217a17c.

--- AluguelVeiculosUTF-Cliente/WindowsFormsApplication1/Program.cs
+++ AluguelVeiculosUTF-Cliente/WindowsFormsApplication1/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace AluguelVeiculosUTF_Cliente
+namespace WindowsFormsApplication1
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace AluguelVeiculosUTF_Cliente
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AlugarCarro());
+            Application.Run(new Form1());
         }
     }
 }
