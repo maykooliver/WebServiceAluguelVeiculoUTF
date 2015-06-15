ea57423a6e37e41d0781a71cfe70f3d97183b335
maykooliver <maykooliver@gmail.com>
2015-06-15 18:53:53 -0300

    Atualizando Telas C#

--- AluguelVeiculosUTF-Cliente/AluguelVeiculosUTF-Cliente/AlugarCarro.cs
+++ AluguelVeiculosUTF-Cliente/AluguelVeiculosUTF-Cliente/AlugarCarro.cs
@@ -17,27 +17,7 @@ namespace AluguelVeiculosUTF_Cliente
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox7_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void AlugarCarro_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label7_Click(object sender, EventArgs e)
         {
 
         }
